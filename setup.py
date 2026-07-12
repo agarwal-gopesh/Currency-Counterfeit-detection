9de9872439cac6e29fe065ac4cbb8ae348da07b4
Gopesh Agarwal <agarwalgopesh6@gmail.com>
2026-07-12 14:47:10 +0530

    Initial commit , data is tracked by DVC , setup a local storage for dvc

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project classifies between real and fake indian currency notes with their denomination.',
+    author='Gopesh',
+    license='MIT',
+)
